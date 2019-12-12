9ad78ac1bfc4e1027e3d14969016912f96e02433
mwong01 <mattw90@gmail.com>
2019-12-12 22:38:15 +0000

    user authentication v1

---
+++ app/controllers/admin/application_controller.rb
@@ -0,0 +1,9 @@
+class Admin::ApplicationController < ApplicationController
+http_basic_authenticate_with name: ENV["ADMIN_USERNAME"], password: ENV["ADMIN_PASSWORD"]
+
+
+
+before_filter :authorize
+
+
+end
--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,7 +1,5 @@
-class Admin::ProductsController < ApplicationController
-  http_basic_authenticate_with name: ENV["ADMIN_USERNAME"], password: ENV["ADMIN_PASSWORD"]
-
-
+class Admin::ProductsController < Admin::ApplicationController
+  
   def index
     @products = Product.order(id: :desc).all
   end
